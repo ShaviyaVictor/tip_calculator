79ca2d509993d24ff22ba373917fe8a14677495f
shaviyavictor <shaviyavictor@gmail.com>
2022-05-21 16:10:07 +0300

    Create variable to hold the form inputted values

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,7 @@
 // global function variable  to be called in the form action
 var calc = function() {
 
-  
+  var bill = Number(document.getElementById('bill').value);
+  var split = Number(document.getElementById('split').value)
 
 }
