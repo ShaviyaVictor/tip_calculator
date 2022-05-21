952799eae13e9fcf15580da2cbfe995686250eb6
shaviyavictor <shaviyavictor@gmail.com>
2022-05-21 16:11:09 +0300

    Create variables that perform the calculations

--- assets/logic.js
+++ assets/logic.js
@@ -3,5 +3,9 @@ var calc = function() {
 
   var bill = Number(document.getElementById('bill').value);
   var split = Number(document.getElementById('split').value)
+  
+  var tip = bill * 0.15;
+  var totalBill = bill + tip;
+  var splitPerPerson = totalBill / split;
 
 }
